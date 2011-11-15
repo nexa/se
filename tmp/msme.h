67187dc14136126ae4be9d16d373ba5b33a1a780
nexa <mcyzlizhun@hotmail.com>
2011-11-15 23:46:29 +0800

    update

--- tmp/msme.h
+++ tmp/msme.h
@@ -8,19 +8,27 @@
 #ifndef _MSME_H_
 #define _MSME_H_
 
-typedef enum _STATE
+typedef enum _NODE_STATE
   {
     DEFAULT = 0,
     MATCHING,
     PENDING,
     MATCHED,
-  }STATE;
+  }NODE_STATE;
+
+typedef enum _MSME_STATE
+  {
+    UNINITIALIZED = 0,
+    INITIALIZED,
+    COMPILED,
+    DISMISSED,
+  }MSME_STATE;
 
 typedef enum _TYPE
   {
-    ROOT = 0,
-    BRANCH,
-    LEAF,
+    ROOT = 0x1,
+    BRANCH = 0x2,
+    LEAF = 0x4,
   }TYPE;
 
 typedef enum _MODE
@@ -34,12 +42,12 @@ typedef struct node_s
   int id;
   int beg;
   int end;
-  unsigned char *child_keys;
+  unsigned char *child_ids;
   struct node_s **child_ptrs;
   int child_count;
   int child_count_max;
   MODE mode;
-  STATE state;
+  NODE_STATE state;
   TYPE type;
   struct node_s *parent_ptr;
 }node_t, *NPTR;
@@ -52,6 +60,7 @@ typedef struct msme_s
   unsigned char capility;
   node_t *root;
   MODE mode;
+  MSME_STATE state;
 }msme_t;
 
 int node_init(node_t *node, int child_count_max);
@@ -62,5 +71,6 @@ node_t * node_fork(node_t *node);
 int msme_init(msme_t *msme, char **strings, unsigned char string_count);
 void msme_uninit(msme_t *msme);
 int msme_compile(msme_t *msme);
+void msme_dismiss(msme_t *msme);
 
 #endif
